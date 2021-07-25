6ea9f3f829a17596fae6a324361afb8268c96a70
Kubi Kubisiak <mckubisiak@gmail.com>
2021-07-25 15:19:45 -0700

    controller written and passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Marble from '..lib/models/marble.js';
+import Marble from '../lib/models/marble.js';
 
 describe('marble routes', () => {
   beforeEach(() => {
@@ -22,20 +22,20 @@ describe('marble routes', () => {
       id: '1',
       ...earth
     });
-
-    it('GET a single marble', async () => {
-      const earth = await Marble.insert({ 
-        name: 'Earth', 
-        cost: 5, 
-        description: 'magnificant object, terrible inhabitants' 
-      });
+  });
   
-      const res = await request(app).get(`/api/v1/marbles/${earth.id}`).get(earth);
-  
-      expect(res.body).toEqual({
-        id: '1',
-        ...earth
-      });
+  it('GET a single marble', async () => {
+    const earth = await Marble.insert({ 
+      name: 'Earth', 
+      cost: 5, 
+      description: 'magnificant object, terrible inhabitants' 
+    });
+    
+    const res = await request(app).get(`/api/v1/marbles/${earth.id}`);
+    
+    expect(res.body).toEqual({
+      id: '1',
+      ...earth
     });
   });
 });
